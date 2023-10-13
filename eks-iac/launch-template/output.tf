9b4470b95dd8f3c1ab1abf61e797c7e16b0f494d
vicky <vicky210@gmail.com>
2023-10-13 13:18:40 +0530

    added op

--- eks-iac/launch-template/output.tf
+++ eks-iac/launch-template/output.tf
@@ -1,6 +1,12 @@
-output "eksngdepends" {
+output "lt_name" {
+  value = "${aws_launch_template.lteks.name}"
+}
+output "lt_version" {
+  value = "${aws_launch_template.lteks.latest_version}"
+}
+output "ltdepends" {
   value = {}
   depends_on = [
-    aws_eks_node_group.ssc_eks_ng
+    aws_launch_template.lteks
   ]
 }
